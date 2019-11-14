10f962131c5d2883203e0caf17f30dd860f7910b
malay <tomalaypandey.tanu@gmail.com>
2019-11-14 22:05:48 +0530

    Structure of ANN

--- ann.py
+++ ann.py
@@ -1,1 +1,21 @@
+import numpy as np
+import math
 
+class ANN:
+    def __init__(self):
+        pass
+
+    def forward_propogation(self):
+        pass
+
+    def backpropogation(self):
+        pass
+
+    def update_weight(self):
+        pass
+
+    def train(self):
+        pass
+
+class Layer:
+    pass
