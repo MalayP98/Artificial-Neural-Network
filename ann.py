04a7abb6f31c19e0817c0c6c1a8ebd0812fe370f
malay <tomalaypandey.tanu@gmail.com>
2019-11-15 00:09:16 +0530

    Structure of ANN

--- ann.py
+++ ann.py
@@ -1,9 +1,18 @@
 import numpy as np
 import math
 
+
 class ANN:
-    def __init__(self):
-        pass
+    def __init__(self, define):
+        self.define = define
+        self.layer_list = []
+        for i in range(len(self.define)):
+            if i == 0:
+                layer = Layer(self.define[i],1)
+                self.layer_list.append(layer)
+            else:
+                layer = Layer(self.define[i], self.define[i-1])
+                self.layer_list.append(layer)
 
     def forward_propogation(self):
         pass
@@ -17,5 +26,15 @@ class ANN:
     def train(self):
         pass
 
+
 class Layer:
-    pass
+    def __init__(self, neurons, previous_layer_neurons):
+        self.neurons = neurons
+        self.inputs = [0] * previous_layer_neurons
+        self.outputs = [0] * neurons
+        self.weights = np.empty((neurons, previous_layer_neurons))
+
+
+obj = ANN([3,1,1])
+for i in range(len(obj.define)):
+    print(obj.layer_list[i].weights, "\n")
